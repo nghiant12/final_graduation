7c98a41a2ada14d992cffeba18fc10a36f9fd09c
Thu Nghia <maymimim.1202@gmail.com>
2024-12-30 15:14:42 +0700

    Initial commit

---
+++ src/main/java/org/example/final_graduation/FinalGraduationApplication.java
@@ -0,0 +1,13 @@
+package org.example.final_graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalGraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalGraduationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/final_graduation/FinalGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.final_graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalGraduationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
